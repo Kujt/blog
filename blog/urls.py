544c812e16cfdc1701baed3c2e71db0cdfcac4d2
Kujtim Krasniqi <kujtim54@hotmail.com>
2024-11-29 01:38:52 +0100

    I did implement views.py

--- blog/urls.py
+++ blog/urls.py
@@ -13,9 +13,22 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+# from . import views
+
+app_name = "blog"
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
+    # Post views
+    # path("", views.post_list, name="post_list"),
+    # path(
+    #     "<int:year>/<int:month>/<int:day>/<slug:post>/",
+    #     views.post_detail,
+    #     name="post_detail",
+    # ),
+    path("blog/", include("blog.urls", namespace="blog")),
 ]
--- blog_app/models.py
+++ blog_app/models.py
@@ -3,6 +3,7 @@ from django.db import models
 # Create your models here.
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 
 class PublishedManager(models.Manager):
@@ -34,3 +35,9 @@ class Post(models.Model):
 
     objects = models.Manager()  # The default manager.
     published = PublishedManager()  # Our custom manager.
+
+    def get_absolute_url(self):
+        return reverse(
+            "blog:post_detail",
+            args=[self.publish.year, self.publish.month, self.publish.day, self.slug],
+        )
