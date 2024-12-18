cc9996e23c0165c69c3216d099258af1674ce1d2
Kujtim Krasniqi <kujtim54@hotmail.com>
2024-12-18 20:17:54 +0100

    Adding comments to the post detail template

--- blog/views.py
+++ blog/views.py
@@ -71,19 +71,21 @@ def post_detail(request, year, month, day, post):
     )
     # List of active comments for this post
     comments = post.comments.filter(active=True)
+
     new_comment = None
+
     if request.method == "POST":
         # A comment was posted
         comment_form = CommentForm(data=request.POST)
         if comment_form.is_valid():
-            # Create comment object but don't save tp database yet
+            # Create Comment object but don't save to database yet
             new_comment = comment_form.save(commit=False)
             # Assign the current post to the comment
             new_comment.post = post
             # Save the comment to the database
             new_comment.save()
-        else:
-            comment_form = CommentForm()
+    else:
+        comment_form = CommentForm()
 
     return render(
         request,
