a2273309c07c99c9e6e06a357f5d64bc0a02510e
Kujtim Krasniqi <kujtim54@hotmail.com>
2024-12-19 11:52:40 +0100

    Fixing the error of pagination and tags

--- blog/views.py
+++ blog/views.py
@@ -14,51 +14,26 @@ load_dotenv()
 EMAIL = os.environ.get("EMAIL_HOST_USER")
 
 
-class PostListView(ListView):
-    queryset = Post.published.all()
-    context_object_name = "posts"
-    paginate_by = 3
-    template_name = "blog/post/list.html"
+def post_list(request, tag_slug=None):
+    object_list = Post.published.all()
+    tag = None
 
-
-def post_share(request, post_id):
-    # Retrieve post by id
-    post = get_object_or_404(Post, id=post_id, status="published")
-    sent = False
-    if request.method == "POST":
-        form = EmailPostForm(request.POST)
-        if form.is_valid():
-            # Form fields passed validation
-            cd = form.cleaned_data
-            post_url = request.build_absolute_uri(post.get_absolute_url())
-            subject = f"{cd['name']} recommends you read {post.title}"
-            message = (
-                f"Read {post.title} at {post_url}\n\n"
-                f"{cd['name']}'s comments: {cd['comments']}"
-            )
-            send_mail(subject, message, EMAIL, [cd["to"]])
-            sent = True
-    else:
-        form = EmailPostForm()
+    if tag_slug:
+        tag = get_object_or_404(Tag, slug=tag_slug)
+        object_list = object_list.filter(tags__in=[tag])
+    paginator = Paginator(object_list, 3)
+    page = request.GET.get("page")
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer deliver the first page
+        posts = paginator.page(1)
+    except EmptyPage:
+        # if page is out of range deliver last page of results
+        posts = paginator.page(paginator.num_pages)
     return render(
-        request, "blog/post/share.html", {"post": post, "form": form, "sent": sent}
+        request, "blog/post/list.html", {"page": page, "posts": posts, "tag": tag}
     )
-
-
-# def post_list(request):
-#     posts = Post.published.all()
-#     paginator = Paginator(posts, 3)  # 3 posts in each page
-#     page = request.GET.get("page")
-#     try:
-#         posts = paginator.page(page)
-#     except PageNotAnInteger:
-#         # If page is not an integer deliver the first page
-#         posts = paginator.page(1)
-#     except EmptyPage:
-#         # If page is out of range deliver last page of results
-#         posts = paginator.page(paginator.num_pages)
-
-#     return render(request, "blog/post/list.html", {"posts": posts})
 
 
 def post_detail(request, year, month, day, post):
@@ -100,23 +75,32 @@ def post_detail(request, year, month, day, post):
     )
 
 
-def post_list(request, tag_slug=None):
-    object_list = Post.published.all()
-    tag = None
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = "posts"
+    paginate_by = 3
+    template_name = "blog/post/list.html"
 
-    if tag_slug:
-        tag = get_object_or_404(Tag, slug=tag_slug)
-        object_list = object_list.filter(tags__in=[tag])
-    paginator = Paginator(object_list, 3)
-    page = request.GET.get("page")
-    try:
-        posts = paginator.page(page)
-    except PageNotAnInteger:
-        # If page is not an integer deliver the first page
-        posts = paginator.page(1)
-    except EmptyPage:
-        # if page is out of range deliver last page of results
-        posts = paginator.page(paginator.num_pages)
+
+def post_share(request, post_id):
+    # Retrieve post by id
+    post = get_object_or_404(Post, id=post_id, status="published")
+    sent = False
+    if request.method == "POST":
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            # Form fields passed validation
+            cd = form.cleaned_data
+            post_url = request.build_absolute_uri(post.get_absolute_url())
+            subject = f"{cd['name']} recommends you read {post.title}"
+            message = (
+                f"Read {post.title} at {post_url}\n\n"
+                f"{cd['name']}'s comments: {cd['comments']}"
+            )
+            send_mail(subject, message, EMAIL, [cd["to"]])
+            sent = True
+    else:
+        form = EmailPostForm()
     return render(
-        request, "blog/post/list.html", {"page": page, "posts": posts, "tag": tag}
+        request, "blog/post/share.html", {"post": post, "form": form, "sent": sent}
     )
