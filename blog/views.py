6ece76e02ba03534e5d399b7659786f9e4a313ae
Kujtim Krasniqi <kujtim54@hotmail.com>
2024-12-24 22:09:35 +0100

    Building a search view

--- blog/views.py
+++ blog/views.py
@@ -11,6 +11,8 @@ from taggit.models import Tag  # type: ignore
 from django.db.models import Count
 import os
 from dotenv import load_dotenv  # type: ignore
+from django.contrib.postgres.search import SearchVector
+from .forms import CommentForm, EmailPostForm, SearchForm
 
 load_dotenv()
 EMAIL = os.environ.get("EMAIL_HOST_USER")
@@ -114,4 +116,23 @@ def post_share(request, post_id):
         form = EmailPostForm()
     return render(
         request, "blog/post/share.html", {"post": post, "form": form, "sent": sent}
+    )
+
+
+def post_search(request):
+    form = SearchForm()
+    query = None
+    results = []
+
+    if "query" in request.GET:
+        form = SearchForm(request.GET)
+        if form.is_valid():
+            query = form.cleaned_data["query"]
+            results = (
+                Post.published.annotate(search=SearchVector("title", "body"))
+            ).filter(search=query)
+    return render(
+        request,
+        "blog/post/search.html",
+        {"form": form, "query": query, "results": results},
     )
